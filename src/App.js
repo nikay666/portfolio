857ccd214544c7550ef56ffe3c573b8b7ebd62b5
Nika Yakubova <lilit666an@gmail.com>
2020-10-23 19:08:21 +0300

    initial react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  const classes = useStyles() 
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
