3f724dee509dfcd3f64aa913ce2048749a19c5f5
Nika Yakubova <lilit666an@gmail.com>
2020-11-02 23:59:44 +0300

    Исправлены: родительский тег компонента Card, названия  классов оболочек  внутри Card, стили к  ним

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,30 +5,30 @@ import './Card.scss'
 const Card = ({url, title, href, link, text, tags }) => {
 
     return (
-        <div className='card' >
+        <a href={href} className='card card--link' >
             
             <div className="card__font">
                 <img className="card__font-img" src={url} alt={title}/>
             </div>
 
             <div className="card__back">
-                <div className="card__back-top">
+                <div className="card__wrap">
                     <div className="card__title">
                     <h3 className="card__back-title">{title}</h3>
-                    <a className="card__back-link" href={href}>{link}</a>
+                    {/* <a className="card__back-link" href={href}>{link}</a> */}
                 </div>
 
-                <div className="card__block">
-                    <h4 className="card__block-title">Описание сайта: </h4>
-                    <p className="card__block-text">{text}</p>
+                <div className="card__wrap">
+                    <h4 className="card__wrap-title">Описание сайта: </h4>
+                    <p className="card__wrap-text">{text}</p>
                 </div>
                 </div>
 
-            <div className="card__block">
-                <h4 className="card__block-title">Технологии: </h4>
-                <ul className="card__block-tags">
+            <div className="card__wrap">
+                <h4 className="card__wrap-title">Технологии: </h4>
+                <ul className="tags">
                     {tags.map(( tag, index) => (
-                        <li className="card__block-tag"
+                        <li className="tag"
                             key={index}
                             data-tag={tag}
                         >{tag}</li>
@@ -37,7 +37,7 @@ const Card = ({url, title, href, link, text, tags }) => {
             </div>
 
         </div>
-        </div>
+        </a>
     )
 }
 
