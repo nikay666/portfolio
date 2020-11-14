4f9dab65403bcafe6e35e931c71e2c9209c603e7
Nika Yakubova <lilit666an@gmail.com>
2020-11-15 00:32:16 +0300

    Меню в Categories

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Button.scss'
 
-const Button = ({children, classes}) => {
+const getClasses = (classes) =>  {
     const classNames = ['button']
     classes && classNames.push(classes)
-    const className =  classNames.join(' ')
+    const className = classNames.join(' ')
+
+    return className
+}
+
+const Button = ({children, classes, onClick = null}) => {
+    const className = getClasses(classes)
+
+    const onClickEvent = (e) => {
+        onClick && onClick(e)
+    }
 
     return (
-        <button className={className}>
+        <button 
+            onClick={onClickEvent}
+            className={className}
+        >
             {children}
         </button>
     )
--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,17 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import { HeaderLink } from '../../components/HeaderLink'
 import './Header.scss'
 
 
+const activeClass = 'active'
+
 const toggleNavMenu = (nav, active) =>  {
      //overflow-hidden - to disable scrolling
     if(active) {
         document.body.classList.remove('overflow-hidden')
-        nav.current.classList.remove('active')
+        nav.current.classList.remove(activeClass)
     } else {
         document.body.classList.add('overflow-hidden')
-        nav.current.classList.add('active') 
+        nav.current.classList.add(activeClass) 
     }
 }
 
@@ -20,10 +22,14 @@ const Header = ({links}) => {
     const [activeBurger, setActiveBurger] = useState(false)
     const navMenu = useRef()
 
+    useEffect(() => {
+        navMenu.current.classList.contains(activeClass) &&  navMenu.current.classList.remove(activeClass)
+    }, [])
+
     const handlerActiveBurger = (e) => {
         setActiveBurger(!activeBurger)
 
-        e.target.classList.toggle('active')
+        e.target.classList.toggle(activeClass)
         toggleNavMenu(navMenu, activeBurger)
     }
 
@@ -43,6 +49,7 @@ const Header = ({links}) => {
                 </ul>
             </nav>
             <button 
+                aria-label='Меню'
                 className='btn-burger'
                 onClick={handlerActiveBurger}
             ><span className='btn-burger__line'></span></button>
